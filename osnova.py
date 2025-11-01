4928639eb145728debbb95c091f1120fb0082e4d
герман <germanvilkov999@gmail.com>
2025-11-01 18:44:48 +0500

    Add addition

--- osnova.py
+++ osnova.py
@@ -2,3 +2,8 @@ while True:
     cmd=input("Выберите один из знаков> ")
     if cmd=="exit":
         break
+
+    elif cmd == "+":
+        a = int(input())
+        b = int(input())
+        print(a+b)
