c3d35f0fd3823d6a80ff65f14991288474cf83e3
sikot <sikotov2010@gmail.com>
2025-11-09 19:47:37 +0500

    Добавил косинус и степень

--- osnova.py
+++ osnova.py
@@ -1,4 +1,12 @@
+import math
 while True:
     cmd=input("Выберите один из знаков> ")
     if cmd=="exit":
         break
+    elif cmd=="степень":
+        a=int(input("Введите число,возводимое в степень "))
+        b=int(input("Введите степень "))
+        print(a**b)
+    elif cmd=="косинус":
+        a=int(input())
+        print(math.cos(a))
